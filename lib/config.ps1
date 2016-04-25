bc171ed10e265a93490c6df31f12a3b1daecb07b
jshurak <jshurak@gmail.com>
2016-04-25 11:15:22 -0400

    config working

--- lib/config.ps1
+++ lib/config.ps1
@@ -1,3 +1,4 @@
 ﻿$Global:ConfigDirectory = "C:\Users\jshurak\Documents\GitHub\SQL-Assessment\lib"
 $Global:QueryDirectory = "C:\Users\jshurak\Documents\GitHub\SQL-Assessment\queries"
 $Global:InstanceList = get-content "$Global:ConfigDirectory\InstanceList.txt"
+
